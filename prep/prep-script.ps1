d8d2753bb1130ea2e8dcd6662ea43acfbe104dd1
Michael Heydt <mike@heydt.org>
2020-01-27 11:09:40 -0700

    updates to prep script

--- prep/prep-script.ps1
+++ prep/prep-script.ps1
@@ -1,13 +1,12 @@
-npm i -g azure-functions-core-tools@3 --unsafe-perm true
-choco install postman -y
-choco install sql-server-express -y
-choco install azurestorageemulator -y
-choco install cosmosdbexplorer -y
-choco install azure-data-studio -y
-cmd /c 'C:\Program Files (x86)\Microsoft SDKs\Azure\Storage Emulator\AzureStorageEmulator.exe' start
-code --install-extension ms-vscode.csharp
-code --install-extension ms-vscode.azurecli
-code --install-extension ms-azuretools.vscode-azurefunctions
-code --install-extension ms-vscode.azure-account
-code --install-extension ms-azuretools.vscode-azurestorage
-code --install-extension ms-vscode.powershell
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install nodejs-lts -y --force
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+npm i -g azure-functions-core-tools@3 --unsafe-perm true >> afctlog.txt
+#Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -force
+#Install-Module -Name Az -AllowClobber -Scope AllUsers -force
+code --install-extension ms-vscode.csharp >> vscodelog.txt
+code --install-extension ms-vscode.azurecli >> vscodelog.txt
+code --install-extension ms-azuretools.vscode-azurefunctions >> vscodelog.txt
+code --install-extension ms-vscode.azure-account >> vscodelog.txt
+code --install-extension ms-azuretools.vscode-azurestorage >> vscodelog.txt
+code --install-extension ms-vscode.powershell >> vscodelog.txt
