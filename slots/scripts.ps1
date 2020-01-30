6df85bf052d3ae1d30ed8ab1d3783fe3fd157d24
Michael Heydt <mike@heydt.org>
2020-01-30 16:30:34 -0700

    lab updates

--- slots/scripts.ps1
+++ slots/scripts.ps1
@@ -1,24 +1,25 @@
 az login
 
-$funcappname = $(az functionapp list --query "name" -o tsv)
+$funcappname = $(az functionapp list --query "[0].name" -o tsv)
 $funcappname
 
-$group = $(az group list --query "name" -o tsv)
+$group = $(az group list --query "[0].name" -o tsv)
+$group 
 
-az functionapp deployment slot create --name $funcappname -g $group
+az functionapp deployment slot create --name $funcappname -g $group --slot production
 az functionapp deployment slot create --name $funcappname -g $group --slot staging
 
 # modify code (A)
 dotnet clean
 dotnet build
 
-func azure functionapp publish $funcappname
+func azure functionapp publish $funcappname --force
 
 # modify code (B)
 dotnet clean
 dotnet build
 
-func azure functionapp publish $funcappname --slot staging 
+func azure functionapp publish $funcappname --slot staging --force
 
 # swap B into prod, A into staging
 az functionapp deployment slot swap -g $group -n $funcappname --slot staging --target-slot production 
